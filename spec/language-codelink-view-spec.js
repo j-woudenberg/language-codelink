a939b2f02b90581ae1ce0d14d052e6c46c28c512
j.woudenberg <j.woudenberg@linkeng.com>
2020-08-04 13:43:51 -0400

    Init commit of CodeLink grammar

---
+++ spec/language-codelink-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageCodelinkView from '../lib/language-codelink-view';
+
+describe('LanguageCodelinkView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
